c1c06d9bffa0a594526883bbff875dcbcc330872
lukastff <lucastavaresffr@gmail.com>
2025-10-16 08:58:16 -0300

    feat: add product creation endpoint with validation

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -1,9 +1,25 @@
 import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
 
 class ProductController {
     async index(req: Request, res: Response, next: NextFunction) {
         try {
             return res.json({ message: "Ok" });
+        } catch (error) {
+            return next(error);
+        }
+    }
+
+    async create(req: Request, res: Response, next: NextFunction) {
+        try {
+            const bodySchema = z.object({
+                name: z.string().trim().min(6),
+                price: z.number().gt(0),
+            });
+
+            const { name, price } = bodySchema.parse(req.body);
+
+            return res.status(201).json({ name, price });
         } catch (error) {
             return next(error);
         }
