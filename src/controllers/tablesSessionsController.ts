fdd6a5e3a430f4cf8a0c0b9214d94e3374dde60f
lukastff <lucastavaresffr@gmail.com>
2025-10-17 01:32:13 -0300

    feat: add tables sessions validation and repository

--- src/controllers/tablesSessionsController.ts
+++ src/controllers/tablesSessionsController.ts
@@ -14,6 +14,28 @@ class TablesSessionsController {
 
     async create(req: Request, res: Response, next: NextFunction) {
         try {
+            const bodySchema = z.object({
+                table_id: z.number(),
+            });
+
+            const { table_id } = bodySchema.parse(req.body);
+
+            const session = await knex<TablesSessionsRepository>(
+                "tables_sessions"
+            )
+                .where({ table_id })
+                .orderBy("opened_at", "desc")
+                .first();
+
+            if (session && !session.closed_at) {
+                throw new AppError("This table is already occupied");
+            }
+
+            await knex<TablesSessionsRepository>("tables_sessions").insert({
+                table_id,
+                opened_at: knex.fn.now(),
+            });
+
             return res.status(201).json();
         } catch (error) {
             return next(error);
