cdc96c72561a1a604b521c12fe47316fc33c3205
lukastff <lucastavaresffr@gmail.com>
2025-10-17 13:44:42 -0300

    feat: add orders listing endpoint

--- src/routes/ordersRoutes.ts
+++ src/routes/ordersRoutes.ts
@@ -4,6 +4,7 @@ import { Router } from "express";
 const ordersRoutes = Router();
 const ordersController = new OrdersController();
 
+ordersRoutes.get("/table-session/:id", ordersController.index);
 ordersRoutes.post("/", ordersController.create);
 
 export { ordersRoutes };
